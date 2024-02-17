b9918b640b79d7bd28cc55d0fa4617ff678637a0
Thomas Yuan <thomasyuan1@gmail.com>
2024-02-17 14:03:52 +0000

    debug

--- experiments/run_experiments_acflow.py
+++ experiments/run_experiments_acflow.py
@@ -61,6 +61,9 @@ def main(
     logging.debug(
         f"Applying transformations..."
     )
+    
+    import pdb
+    pdb.set_trace()
     train_dl = transform_dataloader(train_dl, n_tasks)
     # For now, we assume that all concepts have the same
     # aquisition cost

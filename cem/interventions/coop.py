7fe70d51d1070da81822453b71bfa36ec4efab0a
Thomas Yuan <thomasyuan1@gmail.com>
2024-02-18 02:30:31 +0000

    debug

--- cem/interventions/coop.py
+++ cem/interventions/coop.py
@@ -165,7 +165,7 @@ class CooP(InterventionPolicy):
                 # And aim to maximize the probability of the ground-truth class
                 # assuming we also correctly intervened on the current concept
                 expected_change = new_prob_distr[
-                    torch.eye(new_prob_distr.shape[-1])[pred_class].type(
+                    torch.eye(new_prob_distr.shape[-1]).to(new_prob_distr.device)[pred_class].type(
                         torch.bool
                     )
                 ]

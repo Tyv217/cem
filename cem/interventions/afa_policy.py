60f068bc643a9000224ca3bf006dd1bd91c37675
Thomas Yuan <thomasyuan1@gmail.com>
2024-03-04 18:46:52 +0000

    rl policy

--- cem/interventions/afa_policy.py
+++ cem/interventions/afa_policy.py
@@ -7,18 +7,23 @@ from gymnasium import spaces
 
 class AFAEnv(gym.Env):
 
-    def __init__(self, cbm, n_concepts, n_tasks, seed, use_groups, concept_group_map = None):
+    def __init__(self, cbm, n_concepts, n_tasks, seed, use_concept_groups, concept_group_map = None, emb_size = 1):
 
         self.cbm = cbm
         self.n_concept = len(concept_group_map) if use_groups else n_concepts
         self.n_tasks = n_tasks
         self.seed = seed
+        self.use_concept_groups = use_concept_groups
+        self.concept_group_map = concept_group_map
+        self.emb_size = 1
 
         self.observation_space = spaces.Dict(
             {
                 "intervened_concepts": spaces.MultiBinary(self.n_concepts, seed=self.seed),
-                "flow_model_output": spaces.Box(shape = [self.n_concepts], dtype = np.float32, seed=self.seed),
-                "cbm_model_output": spaces.Box(shape = [self.n_concepts], dtype = np.float32, seed=self.seed)
+                "ac_model_output": spaces.Box(shape = [self.n_concepts], dtype = np.float32, seed=self.seed),
+                "cbm_bottleneck": spaces.Box(shape = [self.n_concepts * self.emb_size], dtype = np.float32, seed=self.seed),
+                "cbm_pred_concepts": spaces.Box(shape = [self.n_concepts * self.emb_size], dtype = np.float32, seed=self.seed),
+                "cbm_pred_output": spaces.Discrete(shape = [self.n_tasks], seed=self.seed),
             }
         )
 
@@ -27,5 +32,13 @@ class AFAEnv(gym.Env):
     def _get_obs(self):
         return {
             "intervened_concepts": self.intervened_concepts,
-            "flow_model_output": self._target_location
+            "ac_model_output": self.ac_model_output,
+            "cbm_bottleneck": self.cbm_bottleneck,
+            "cbm_pred_concepts": self.cbm_pred_concepts,
+            "cbm_pred_output": self.cbm_pred_output,
+        }
+
+    def _get_info(self):
+        return {
+            "intervened_concepts": self.intervened_concepts
         }

c2923035cc4c0823a01448963f6377364a0c07b8
Thomas Yuan <thomasyuan1@gmail.com>
2024-03-11 22:39:37 +0000

    set correct save path

--- experiments/run_experiments_acflow_cem.py
+++ experiments/run_experiments_acflow_cem.py
@@ -267,7 +267,11 @@ def main(
                         full_run_name=full_run_name
                     )
 
-                    experiment_config['shared_params']['save_path'] = ac_model_saved_path
+                    experiment_config['shared_params']['ac_model_config']['save_path'] = ac_model_saved_path
+
+                    logging.debug(
+                        f"Setting ac model save path to be {ac_model_saved_path}"
+                    )
 
                 if run_config["architecture"] in [
                     "IndependentConceptBottleneckModel",

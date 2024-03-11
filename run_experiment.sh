0934dd4718a78a98539f9167ae8294ebb32bd3f5
Xiaotian Yuan <xty20@login-e-1.data.cluster>
2024-03-11 13:19:24 +0000

    edit epochs

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python experiments/run_experiments_acflow_cem.py -c experiments/configs/afa_configs/mnist_acflow_cem.yaml --debug
+python experiments/run_experiments_acflow.py -c experiments/configs/afa_configs/mnist_acflow.yaml --debug
+# python experiments/run_experiments_acflow_cem.py -c experiments/configs/afa_configs/mnist_acflow_cem.yaml --debug
+

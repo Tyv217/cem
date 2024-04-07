1b2d0dfecfaee1585c5658ed185225192cb3604b
Thomas Yuan <thomasyuan1@gmail.com>
2024-04-07 13:50:26 +0100

    yes

--- cem/models/acenergy.py
+++ cem/models/acenergy.py
@@ -276,7 +276,8 @@ class ACEnergy(pl.LightningModule):
 
     def training_step(self, batch, batch_idx):
         x, b, m, y = batch['x'], batch['b'], batch['m'], batch['y']
-
+        import pdb
+        pdb.set_trace()
         concepts = x * m
 
         energy = self.forward(concepts, train = True)

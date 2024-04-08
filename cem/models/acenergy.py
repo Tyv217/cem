326481b03e181d413b3bf515d51552b1eca1cb44
Thomas <thomasyuan1@gmail.com>
2024-04-08 15:45:37 +0100

    add back pdb

--- cem/models/acenergy.py
+++ cem/models/acenergy.py
@@ -242,6 +242,9 @@ class ACEnergy(pl.LightningModule):
         # p(x_u, x_o | y) = e^(E(x_u + x_o, y)) / sum_y(E(x_u + x_o, y))
 
         # e^(E(x_u + x_o, y))
+        if y is None:
+            import pdb
+            pdb.set_trace()
         
         if y is None:
             # p(c | y)

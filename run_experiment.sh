a73971387b22630ce8753e2a11eb4689a0c87a17
Xiaotian Yuan <xty20@login-p-4.data.cluster>
2024-04-15 00:05:40 +0100

    learning rate

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # python experiments/run_experiments_acflow.py -c experiments/configs/afa_configs/mnist_acflow.yaml --debug
-python experiments/run_experiments_ac.py -c experiments/configs/afa_configs/mnist_acflow_cem.yaml --debug --rerun --project_name="test"
-# python experiments/run_experiments_ac.py -c experiments/configs/afa_configs/mnist_acenergy_cem.yaml --debug --force_cpu
+# python experiments/run_experiments_ac.py -c experiments/configs/afa_configs/mnist_acflow_cem.yaml --debug --rerun --project_name="test"
+python experiments/run_experiments_ac.py -c experiments/configs/afa_configs/mnist_acenergy_cem.yaml --debug --rerun --project_name="test"

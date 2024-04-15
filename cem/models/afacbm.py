68487408c152b2f12107daea5586638077c20cff
Thomas Yuan <thomasyuan1@gmail.com>
2024-04-15 14:52:59 +0100

    gym env

--- cem/models/afacbm.py
+++ cem/models/afacbm.py
@@ -5,6 +5,7 @@ import sklearn.metrics
 import torch
 import logging
 import time
+import gymnasium as gym
 
 from torchvision.models import resnet50
 
@@ -2671,7 +2672,11 @@ class AFAModel(pl.LightningModule):
         else:
             checkpoint_location = "" if "save_path" not in ac_model_config.keys() else f"at {ac_model_config['save_path']} "
             raise ValueError(f"AC{ac_model_config['architecture']} model checkpoint {checkpoint_location}incorrect / not found")
-        self.env = AFAEnv(self.cbm, self.ac_model, afa_model_config)
+        try:
+            self.env = gym.make("cem/models/AFAEnv-v0", cbm = self.cbm, ac_model = self.ac_model, env_config = afa_model_config)
+        except:
+            import pdb
+            pdb.set_trace()
         self.agent = PPOLightningAgent(
             self.env,
             act_fun = afa_model_config["activation_function"],

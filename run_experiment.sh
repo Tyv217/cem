c52f975adb98ef06bffacd6d7a23e3d470d1ed19
Xiaotian Yuan <xty20@login-e-2.data.cluster>
2024-04-15 15:06:46 +0100

    run experiment file

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # python experiments/run_experiments_acflow.py -c experiments/configs/afa_configs/mnist_acflow.yaml --debug
 # python experiments/run_experiments_acflow_cem.py -c experiments/configs/afa_configs/mnist_acflow_cem.yaml --debug
-python experiments/run_experiments_ac.py -c experiments/configs/afa_configs/mnist_acenergy_cem.yaml --debug --force_cpu
+# python experiments/run_experiments_ac.py -c experiments/configs/afa_configs/mnist_acenergy_cem.yaml --debug --force_cpu
+python experiments/run_experiments_rl.py -c experiments/configs/afa_configs/mnist_afa.yaml --debug --force_cpu

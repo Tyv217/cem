d07af8a2c13ed4cfa8e23baa8180adc67f5deccc
Thomas Yuan <thomasyuan1@gmail.com>
2024-04-15 16:02:35 +0100

    change naming

--- cem/models/afacbm.py
+++ cem/models/afacbm.py
@@ -2673,7 +2673,7 @@ class AFAModel(pl.LightningModule):
         else:
             checkpoint_location = "" if "save_path" not in ac_model_config.keys() else f"at {ac_model_config['save_path']} "
             raise ValueError(f"AC{ac_model_config['architecture']} model checkpoint {checkpoint_location}incorrect / not found")
-        self.num_envs = config["afa_config"]["num_envs"]
+        self.num_envs = config["afa_model_config"]["num_envs"]
         self.env = gym.vector.make("cem/AFAEnv-v0", num_envs = self.num_envs, cbm = self.cbm, ac_model = self.ac_model, env_config = config)
         import pdb
         pdb.set_trace()
